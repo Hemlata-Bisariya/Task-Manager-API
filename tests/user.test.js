abd21eb2cdb7c92389120914549fc2a1ec9b35fd
Hemlata-Bisariya <hemlata18oct@gmail.com>
2021-07-05 14:46:15 -0900

    some files added

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,14 +1,11 @@
 const request = require("supertest");
 const app = require("../app");
 
-// test('Should signup to  new user', async() => {
-//     await request(app).post("./users").send({
-//         name : "Hemlata",
-//         email: "dimpysingh1818@gmail.com",
-//         password : "Abcdef@18"
-//     }).expect(201);
-// });
+test('Should signup to  new user', async() => {
+    await request(app).post("./users").send({
+        name : "Hemlata",
+        email: "dimpysingh1818@gmail.com",
+        password : "Abcdef@18"
+    }).expect(201);
+});
 
-test('should calculate total with default tip', () => {
-    
-})
